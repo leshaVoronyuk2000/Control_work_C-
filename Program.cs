e1a530eea7774e3891cfd352c3cb4475b334ec79
leshavoronyuk_23 <lesha_voronyuk_2000_voronyuk@mail.ru>
2024-03-11 23:08:54 +1000

    Fourth step

--- Program.cs
+++ Program.cs
@@ -2,10 +2,16 @@
 
 String[] changedArray(String[] arr){
     int newLength = 0;
-    
+
     for(int i = 0;i < arr.Length;i++){
         if(arr[i].Length <= 3){
             newLength = newLength + 1;
         }
     }
+
+    String[] res = new String[newLength];
+    int j = 0;
+
+    
+
 }
